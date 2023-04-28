e2a4ff43b843d33f57d055e5eaad503461509b0f
Vincenzo M <vincenzomaiorana@gmail.com>
2023-04-28 12:08:24 +0200

    impl springboot mvn clean

---
+++ src/main/java/org/vm93/u2w3wp/U2w3wpApplication.java
@@ -0,0 +1,13 @@
+package org.vm93.u2w3wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U2w3wpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U2w3wpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vm93/u2w3wp/U2w3wpApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vm93.u2w3wp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U2w3wpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
