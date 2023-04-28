aceda166f48989f524bc25f10fc3566876ff10e0
Vincenzo M <vincenzomaiorana@gmail.com>
2023-04-28 17:15:44 +0200

    faker_runner for sonda alarms

--- src/main/java/org/vm93/u2w3wp/runner/SondaRunner.java
+++ src/main/java/org/vm93/u2w3wp/runner/SondaRunner.java
@@ -1,19 +1,37 @@
 package org.vm93.u2w3wp.runner;
 
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
+import org.vm93.u2w3wp.model.ControlRoom;
+import org.vm93.u2w3wp.model.Sonda;
 
-import lombok.extern.slf4j.Slf4j;
+import com.github.javafaker.Faker;
 
 @Component
-@Slf4j
 public class SondaRunner implements ApplicationRunner {
 
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 		
+		new Timer().scheduleAtFixedRate(new TimerTask(){
+		    @Override
+		    public void run(){
+		    	Sonda s = randomSonda();
+		    	ControlRoom.alert(s);
+		    }
+		},0,5000);
 
 	}
+	
+	public static Sonda randomSonda() {
+		Faker fake = Faker.instance(new Locale("it-IT"));
+		Sonda s = Sonda.builder().id(fake.number().numberBetween(3l, 10l)).lat(fake.address().latitude()).lon(fake.address().longitude()).smokeLevel(fake.number().numberBetween(0, 10)).build();
+		return s;
+		}
 
 }
