128c57f798fbe3e5b741b93c299ef34b45d32725
Vincenzo M <vincenzomaiorana@gmail.com>
2023-04-28 17:15:05 +0200

    sonda calls controlroom alert system

--- src/main/java/org/vm93/u2w3wp/model/Sonda.java
+++ src/main/java/org/vm93/u2w3wp/model/Sonda.java
@@ -8,30 +8,23 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 @Data
 @Builder
-@Slf4j
 @NoArgsConstructor
 @AllArgsConstructor
 public class Sonda {
-	
+
 	private Long id;
-	private Double lat;
-	private Double lon;
+	private String lat;
+	private String lon;
 	private Integer smokeLevel;
-	
-	public static ResponseEntity<Sonda> alert(Sonda s) throws Exception {
+
+	public ResponseEntity<Sonda> sendStats(Sonda s) throws Exception {
 		try {
 			if (s.getSmokeLevel() > -1 && s.getSmokeLevel() < 11) {
-				if (s.getSmokeLevel() > 5) {
-					log.error("*::ALARM ENGAGED::* > Sonda " + s.getId() + ", LAT=" + s.getLat() + " , LONG=" + s.getLon() + " , SMOKE=" + s.getSmokeLevel() + " < CRITIC!");
-					return new ResponseEntity<>(s, HttpStatus.CREATED);
-				} else {
-					log.info("Sonda " + s.getId() + ", LAT=" + s.getLat() + " , LONG=" + s.getLon() + " , SMOKE=" + s.getSmokeLevel() + " OK");
-					return new ResponseEntity<>(s, HttpStatus.OK);
-				}
+				ControlRoom.alert(s);
+				return new ResponseEntity<>(s, HttpStatus.OK);
 			} else {
 				throw new EvaluationException("Value not valid for SMOKE LEVEL");
 			}
