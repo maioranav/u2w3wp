24f596808a51e5410e672442c23053b02281cf71
Vincenzo M <vincenzomaiorana@gmail.com>
2023-04-29 14:50:06 +0200

    add smokelevel test

--- src/test/java/org/vm93/u2w3wp/U2w3wpApplicationTests.java
+++ src/test/java/org/vm93/u2w3wp/U2w3wpApplicationTests.java
@@ -1,13 +1,27 @@
 package org.vm93.u2w3wp;
 
+import static org.junit.Assert.assertNotNull;
+
+import java.util.Locale;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.vm93.u2w3wp.model.Sonda;
+
+import com.github.javafaker.Faker;
 
 @SpringBootTest
 class U2w3wpApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	public void testSondaFumo() {
+		Faker fake = Faker.instance(new Locale("it-IT"));
+		Sonda s = Sonda.builder().id(fake.number().numberBetween(3l, 10l)).lat(fake.address().latitude()).lon(fake.address().longitude()).smokeLevel(fake.number().numberBetween(0, 10)).build();
+		assertNotNull((Object) s.getSmokeLevel());
 	}
 
 }
